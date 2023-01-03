f18236071b88a596089f953c23a5264f5b0cbeab
Elizabeth <113930754+ElizabethLankford@users.noreply.github.com>
2023-01-03 16:06:40 -0600

    Update index.js
    updated api

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.getElementById("get-activity").addEventListener("click", function() {
-  fetch("https://apis.scrimba.com/bored/api/activity")
+  fetch("https://www.boredapi.com/api/activity/")
     .then(response => response.json())
     .then(data => {
       document.getElementById("activity").innerHTML = data.activity
